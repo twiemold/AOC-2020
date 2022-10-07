2150ab855c2b73179fb28d5422c2d1cdcc1c920c
twiemold <twiemold@uvm.edu>
2022-10-07 16:55:59 -0400

    created find_sums functions. changed from u16 to u32. completed part 1 of day 1

--- aoc-day-1/src/main.rs
+++ aoc-day-1/src/main.rs
@@ -3,13 +3,13 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
 
-fn read_file() -> Result<Vec<u16>, io::Error> {
+fn read_file() -> Result<Vec<u32>, io::Error> {
     let file = File::open("src/input.txt")?;
     let file = BufReader::new(file);
-    let mut nums: Vec<u16> = Vec::new();
+    let mut nums: Vec<u32> = Vec::new();
 
     for line in file.lines() {
-        let new_int = line.unwrap().parse::<u16>().unwrap();
+        let new_int = line.unwrap().parse::<u32>().unwrap();
         nums.push(new_int);
     }
     // for num in &nums {
@@ -19,12 +19,24 @@ fn read_file() -> Result<Vec<u16>, io::Error> {
     Ok(nums)
 }
 
-// fn find_sum(nums: vec<u16>) -> (u16, u16) {
-//     let mut nums_hash = hashmap::new();
-// 
-//     
-// }
+fn find_sum(nums: Vec<u32>, target: u32) -> (u32, u32) {
+    let mut nums_hash: HashMap<u32,u32> = HashMap::new();
+
+    for (i, num) in nums.iter().enumerate() {
+        let diff = target - num;
+        if nums_hash.contains_key(&diff) {
+            return (nums[*nums_hash.get(&diff).unwrap() as usize], nums[i]);
+        } else {
+            nums_hash.insert(*num, i.try_into().unwrap());
+        }
+    }
+
+    (0, 0)
+}
 
 fn main() {
-    let nums = read_file();
+    let nums = read_file().unwrap();
+    let vals = find_sum(nums, 2020);
+    let answer = vals.0*vals.1;
+    println!("The answer is {}", answer);
 }

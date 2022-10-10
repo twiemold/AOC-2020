fa5f3d06df623590a1f80328bbde1afae6dc96ec
twiemold <twiemold@uvm.edu>
2022-10-10 14:25:42 -0400

    Reading file line by line

--- aoc-day-2/src/main.rs
+++ aoc-day-2/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn read_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("src/input.txt")?;
+    let file = BufReader::new(file);
+
+    for line in file.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let num = read_file();
 }

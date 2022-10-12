e6f2c338634630cacdb3e5f4178f30581d500459
twiemold <twiemold@uvm.edu>
2022-10-12 10:37:00 -0400

    Created count_trees function. Passed part 1.

--- aoc-day-3/src/main.rs
+++ aoc-day-3/src/main.rs
@@ -14,9 +14,27 @@ fn read_file() -> Result<Vec<String>, Box<dyn std::error::Error>> {
     Ok(inputs)
 }
 
-fn main() {
-    let processed = read_file().unwrap();
-    for process in processed {
-        println!("{}", process)
+fn count_trees(map: Vec<String>) -> u32 {
+    let mut x = 0;
+    let mut y = 0;
+    let mut tree_count = 0;
+    let width = map[0].len();
+    let length = map.len();
+    while y < length {
+        let mark = map[y].chars().nth(x).unwrap();
+        if mark == '#' {
+            tree_count += 1;
+        }
+        x = (x + 3) % width;
+        y += 1;
     }
+
+    tree_count
+}
+
+fn main() {
+    let map = read_file().unwrap();
+    let tree_count = count_trees(map);
+
+    println!("The tree count is {}", tree_count);
 }

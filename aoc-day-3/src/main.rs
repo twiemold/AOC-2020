6e22f429b7c764ba935badbdcf34fc09dae522c6
twiemold <twiemold@uvm.edu>
2022-10-12 09:39:07 -0400

    Reading in file line by line

--- aoc-day-3/src/main.rs
+++ aoc-day-3/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn read_file() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let file = File::open("src/input.txt")?;
+    let file = BufReader::new(file);
+    let mut inputs: Vec<String> = Vec::new();
+
+    for line in file.lines() {
+        inputs.push(line?);
+    }
+
+    Ok(inputs)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let processed = read_file().unwrap();
+    for process in processed {
+        println!("{}", process)
+    }
 }

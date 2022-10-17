a7402668a1efdc32d3829fe8f8b45c927907dbf1
twiemold <twiemold@uvm.edu>
2022-10-17 13:00:07 -0400

    Fixed bug in process_tickets that was creating duplicate seat IDs. Refactored process_tickets to return max_val and vector of seat_ids. Created find_missing to find missing ticket. Completed part 2. Day 5 finished.

--- aoc-day-5/src/main.rs
+++ aoc-day-5/src/main.rs
@@ -14,48 +14,75 @@ fn read_file(filepath: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> 
     Ok(inputs)
 }
 
-fn process_tickets(tickets: Vec<String>, row_count: u8, column_count: u8) -> u32 {
+fn process_tickets(tickets: Vec<String>, row_count: u8, column_count: u8) -> (Vec<u32>, u32) {
     let mut max_id: u32 = 0;
+    let mut seat_ids: Vec<u32> = Vec::new();
     let row_vec = Vec::from_iter(0..row_count);
     let col_vec = Vec::from_iter(0..column_count);
     for ticket in tickets {
         let mut l = 0;
-        let mut r = row_vec.len();
+        let mut r = row_vec.len()-1;
         let mut mid = 0;
-        for i in 0..7 {
-            mid = (l + r) / 2;
+        for i in 0..6 {
+            mid = l + ((r-l) / 2);
             let letter = ticket.chars().nth(i).unwrap();
             match letter {
                 'F' => r = mid,
-                'B' => l = mid,
+                'B' => l = mid+1,
                 _ => (),
             }
+        }
+        if ticket.chars().nth(6).unwrap() == 'F' {
+            mid = l;
+        } else if ticket.chars().nth(6).unwrap() == 'B' {
+            mid = r;
         }
         let row: u32 = row_vec[mid].into();
         l = 0;
-        r = col_vec.len();
-        mid = 0;
-        for i in 7..10 {
-            mid = (l + r) / 2;
+        r = col_vec.len()-1;
+        for i in 7..9 {
+            mid = l + ((r-l) / 2);
             let letter = ticket.chars().nth(i).unwrap();
             match letter {
                 'L' => r = mid,
-                'R' => l = mid,
+                'R' => l = mid+1,
                 _ => (),
             }
         }
-        let column: u32 = col_vec[mid].into();
-        if row*8+column > max_id {
-            max_id = row*8+column
+        if ticket.chars().nth(9).unwrap() == 'L' {
+            mid = l;
+        } else if ticket.chars().nth(9).unwrap() == 'R' {
+            mid = r;
         }
+        let column: u32 = col_vec[mid].into();
+        // println!("Column: {}", column);
+        let seat_id = row*8+column; 
+        if seat_id  > max_id {
+            max_id = seat_id;
+        }
+        seat_ids.push(seat_id);
 
     }
 
-    max_id
+    (seat_ids, max_id)
+}
+
+fn find_missing(mut seat_ids: Vec<u32>) -> u32 {
+    seat_ids.sort();
+    let mut missing = 0;
+    let mut i = 0;
+    while i < seat_ids.len()-1 && seat_ids[i]+1 == seat_ids[i+1] {
+        i += 1
+    }
+    missing = seat_ids[i]+1;
+    missing
 }
 
 fn main() {
    let tickets = read_file("src/input.txt").unwrap();
-   let max_id = process_tickets(tickets, 127, 7);
-   println!("The max value is {}", max_id);
+   let seat_vals = process_tickets(tickets, 128, 8);
+   let seat_ids = seat_vals.0;
+   let max_id = seat_vals.1;
+   let missing = find_missing(seat_ids);
+   println!("Your ticket id is #{}", missing);
 }

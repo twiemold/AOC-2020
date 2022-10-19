fefcd3c108322aa2e06a6486d69bce8062a5434e
twiemold <twiemold@uvm.edu>
2022-10-19 10:14:04 -0400

    Created sum_questions to count number of unique chars in each group. Passed part 1

--- aoc-day-6/src/main.rs
+++ aoc-day-6/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::{read_to_string};
+use std::collections::HashMap;
 
 fn read_file(filepath: &str) -> Result<String, std::io::Error> {
     let data = read_to_string(filepath)?;
@@ -12,10 +13,26 @@ fn process_data(data: String) -> Vec<String> {
     groups
 }
 
+fn sum_questions(groups: Vec<String>) -> u32 {
+    let mut key_count = 0;
+    for group in groups {
+        let mut sub_count: u32 = 0;
+        let mut group_hash = HashMap::new();
+        let _val = group
+            .chars()
+            .filter(|letter| *letter != '\n')
+            .map(|letter| *group_hash.entry(letter).or_insert(1) += 1)
+            .count();
+        sub_count += group_hash.keys().count() as u32;
+        key_count += sub_count;
+    }
+
+    key_count
+}
+
 fn main() {
     let data = read_file("src/input.txt").unwrap();
     let groups = process_data(data);
-    for group in groups {
-        println!("Group: {}", group);
-    }
+    let sum = sum_questions(groups);
+    println!("The sum of the counts is {}", sum);
 }

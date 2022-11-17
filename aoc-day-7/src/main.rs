1faa9feedbd96aa6184cd71e4497b1aeb598b734
twiemold <twiemold@uvm.edu>
2022-11-16 21:19:57 -0500

    Read in file

--- aoc-day-7/src/main.rs
+++ aoc-day-7/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::{read_to_string};
+
+fn read_file(filepath: &str) -> String {
+    let data = read_to_string(filepath).unwrap();
+
+    data
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = read_file("src/input.txt");
+    println!("{}", data);
 }

30d4d38940b5dc5f3ee021620085d905578d5b89
twiemold <twiemold@uvm.edu>
2022-11-22 21:20:21 -0500

    Reading in data

--- aoc-day-8/src/main.rs
+++ aoc-day-8/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::collections::HashMap;
+
+fn read_file(filepath: &str) -> Vec<String> {
+    let file = File::open(filepath).unwrap();
+    let reader = BufReader::new(file);
+    let mut data = Vec::new();
+
+    for line in reader.lines() {
+        data.push(line.unwrap());
+    }
+
+    data
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = read_file("src/input.txt");
+    for line in data {
+        println!("{line}");
+    }
 }
